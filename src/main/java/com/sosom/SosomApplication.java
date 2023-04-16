66858bf6241b79d9d42f8f164ce44b69e72a55d4
Shin <dbrms1a1a@naver.com>
2023-04-17 01:47:49 +0900

    intial commit

---
+++ src/main/java/com/sosom/SosomApplication.java
@@ -0,0 +1,13 @@
+package com.sosom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SosomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sosom/SosomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
