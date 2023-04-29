c8e1141b83f8637e3eec6be004e582aec4bf1226
Shin <dbrms1a1a@naver.com>
2023-04-29 12:02:54 +0900

    feat: SpringSecurity에서 사용자의 정보를 담거나 가져오는 UserDetails,UserDetailsService 추가
    - 사용자의 정보를 담는 UserDetails 구현
    - DB에서 사용자의 정보를 가져와 UserDetails로 반환하는 UserDetailsService 구현

---
+++ src/main/java/com/sosom/security/userdetails/UserDetailsImpl.java
@@ -0,0 +1,78 @@
+package com.sosom.security.userdetails;
+
+import com.sosom.member.domain.MemberRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails, OAuth2User {
+
+    private String email;
+    private String password;
+    private MemberRole memberRole;
+
+    private Map<String,Object> attributes;
+
+    public UserDetailsImpl(String email,String password,MemberRole memberRole) {
+        this.email = email;
+        this.password = password;
+        this.memberRole = memberRole;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add((GrantedAuthority) () -> String.valueOf(memberRole));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public <A> A getAttribute(String name) {
+        return OAuth2User.super.getAttribute(name);
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+}
