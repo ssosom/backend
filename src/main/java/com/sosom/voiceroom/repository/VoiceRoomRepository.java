3059448e1e4b9db3fa8dcdaccabb31d48b167296
Shin <dbrms1a1a@naver.com>
2023-06-09 15:20:13 +0900

    feat: 대화방에 속한 대화들을 페이징하여 반환하는 기능 추가
    - 회원이 속한 특정한 방의 대화들을 반환하는 api 구현

--- src/main/java/com/sosom/voiceroom/repository/VoiceRoomRepository.java
+++ src/main/java/com/sosom/voiceroom/repository/VoiceRoomRepository.java
@@ -1,7 +1,14 @@
 package com.sosom.voiceroom.repository;
 
+import com.sosom.room.domain.Room;
 import com.sosom.voiceroom.domain.VoiceRoom;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VoiceRoomRepository extends JpaRepository<VoiceRoom,Long> {
+    @Query("select vr from VoiceRoom as vr join fetch vr.voice as v where vr.room = :room order by v.id desc ")
+    Slice<VoiceRoom> findByRoom(@Param("room") Room room, Pageable pageable);
 }
