e85aa8cba435a4025c10222e4fc80b3346e4ebfb
Shin <dbrms1a1a@naver.com>
2023-06-10 11:25:13 +0900

    fix: test가 실패하는 현상 수정
    - @DatajpaTest에서 jpaqueryfactory bean으로 주입으로 버그 수정
    - test DB를 memoryDB로 변경

--- src/test/java/com/sosom/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/sosom/member/repository/MemberRepositoryTest.java
@@ -1,17 +1,22 @@
 package com.sosom.member.repository;
 
+import com.sosom.config.TestConfig;
 import com.sosom.member.domain.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@Import(TestConfig.class)
+@Transactional
 class MemberRepositoryTest {
 
     @Autowired
--- src/test/java/com/sosom/member/service/MemberServiceIntegrationTest.java
+++ src/test/java/com/sosom/member/service/MemberServiceIntegrationTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -29,6 +30,7 @@ import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 public class MemberServiceIntegrationTest {
     @Autowired
     MemberRepository memberRepository;
